80d8bbcb1f908a52c2fdf06af9164d4133dbcff4
Jackred <jackred@tuta.io>
2019-10-17 11:02:34 +0100

    ADD script to create img from row data
    run the file like:
    python create_image.py path_to_csv_file

---
+++ src/create_image.py
@@ -0,0 +1,36 @@
+# -*- Mode: Python; tab-width: 8; indent-tabs-mode: nil; python-indent-offset: 4 -*-
+# vim:set et sts=4 ts=4 tw=80:
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+# author: JackRed <jackred@tuta.io>
+
+import csv
+import sys
+from os import mkdir
+
+IMG_SIZE = 48
+
+
+
+if (len(sys.argv) == 1):
+    sys.exit('toto')
+
+
+
+with open(sys.argv[1]) as csvf:
+    sr = csv.reader(csvf, delimiter=',')
+    imgs = []
+    for row in sr:
+        imgs.append(row)
+
+imgs.pop()
+
+mkdir('test')
+
+for j in range(len(imgs)):
+    with open('./test/test' + str(j) + '.ppm', 'w+') as f:
+        f.write('P3\n48 48 255\n')
+        f.write('\n'.join([str(int(float(i))) + ' ' + str(int(float(i))) + ' ' +
+                           str(int(float(i))) for i in imgs[j]]))
